2bceb739ad8b6673bd6ec691566d6b074ec75c86
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-03-26 23:56:31 -0700

    Built Basic Functionality

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var audio = {
+  "tru": new Audio("sounds/tru.mp3")
+};
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('stopAll').addEventListener('click', function() {
+      for(var prop in audio) {
+        audio[prop].pause();
+        audio[prop].load();
+      }
+    });
+    
+    document.getElementById('tru').addEventListener('click', function() {
+      audio["tru"].play();
+    });
+});
