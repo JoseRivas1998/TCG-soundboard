a30fb5f5d60e227eaddcdb426dcb3fdc09b37d2b
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-03-27 00:43:35 -0700

    Added Sounds and Started Game Grumps

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var audio = {
-  "tru": new Audio("sounds/tru.mp3")
+  "tru": new Audio("sounds/tru.mp3"),
+  "gimme-dat-pussy": new Audio("sounds/gimme-dat-pussy.mp3"),
+  "lemon": new Audio("sounds/lemon.mp3"),
+  "normies": new Audio("sounds/normies.mp3"),
+  "allahu-akbar": new Audio("sounds/allahu-akbar.mp3"),
+  "nobody-gives-a-shit": new Audio("sounds/nobody-gives-a-shit.mp3"),
+  "now-thats-edgy-as-fuck": new Audio("sounds/now-thats-edgy-as-fuck.mp3")
 };
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -10,8 +16,47 @@ document.addEventListener('DOMContentLoaded', function() {
         audio[prop].load();
       }
     });
-    
+
     document.getElementById('tru').addEventListener('click', function() {
+      audio["tru"].pause();
+      audio["tru"].load();
       audio["tru"].play();
     });
+
+    document.getElementById('gimme-dat-pussy').addEventListener('click', function() {
+      audio["gimme-dat-pussy"].pause();
+      audio["gimme-dat-pussy"].load();
+      audio["gimme-dat-pussy"].play();
+    });
+
+    document.getElementById('lemon').addEventListener('click', function() {
+      audio["lemon"].pause();
+      audio["lemon"].load();
+      audio["lemon"].play();
+    });
+
+    document.getElementById('normies').addEventListener('click', function() {
+      audio["normies"].pause();
+      audio["normies"].load();
+      audio["normies"].play();
+    });
+
+    document.getElementById('allahu-akbar').addEventListener('click', function() {
+      audio["allahu-akbar"].pause();
+      audio["allahu-akbar"].load();
+      audio["allahu-akbar"].play();
+    });
+
+    document.getElementById('nobody-gives-a-shit').addEventListener('click', function() {
+      audio["nobody-gives-a-shit"].pause();
+      audio["nobody-gives-a-shit"].load();
+      audio["nobody-gives-a-shit"].play();
+    });
+
+    document.getElementById('now-thats-edgy-as-fuck').addEventListener('click', function() {
+      audio["now-thats-edgy-as-fuck"].pause();
+      audio["now-thats-edgy-as-fuck"].load();
+      audio["now-thats-edgy-as-fuck"].play();
+    });
+
 });
